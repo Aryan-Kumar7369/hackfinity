05fbacafac5a04bab4499f0bc64a6a45e42e9991
Aryan-Kumar7369 <aryankt2006@gmail.com>
2025-01-25 17:32:38 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+document.getElementById("start-click").addEventListener("click", startOptions);
+
+function startOptions() {
+    document.getElementById("start-click").innerHTML = "EARTHQUAKE SIMULATOR";
+    document.getElementById("options-click").innerHTML = "FLOOD SIMULATOR";
+    document.getElementById("about-click").innerHTML = "WILD FIRE SIMULATOR";
+}
+
